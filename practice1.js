b395b1bddcd25cff06476983e1f2b555055337a6
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-07-03 12:01:07 +0530

    first commit

---
+++ practice1.js
@@ -0,0 +1,13 @@
+//CRUD our Objects
+let person = {name: 'John',
+JobTitle: 'Developer',
+email: 'almaBette.com',
+isVarified: false,}
+let newperson = {JobTitle:'Developer',
+email: 'almaBetter.com',
+isVarified: true,
+firstName: 'John',
+lastName: 'Snow'};
+console.log(person.name);
+console.log(person.isVarified);
+console.log(newperson);
