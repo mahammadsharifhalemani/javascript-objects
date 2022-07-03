47ea46e5c86fd00bce5d1101351697dea0e93986
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-07-03 17:49:22 +0530

    second commit

---
+++ practice2.js
@@ -0,0 +1,7 @@
+const userDatails = {name:{first:"Kapil",last:"Raghuwanshi",},
+jobTitle:"JSInstructor@Almabetter.com",
+email:{work:"kapil@google.com", personal:"",},
+status:{isOnline:true,isVarified: false,}}
+userDatails.name = {first:"John", last: "Snow"}
+userDatails.isProMember = false
+console.log(userDatails);
