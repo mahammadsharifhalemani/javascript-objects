92859dd4cd9600094a14933e467558728dfb4d76
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-07-03 17:59:06 +0530

    third commit

---
+++ practice3.js
@@ -0,0 +1,6 @@
+//Destructuring Syntax
+const userActivity3 = {name:"Rahul",jobTitle:"JS instructor@almabetter.com",
+work:"rahul@microsoft.com",
+region:"hyderabad"}
+let {name,jobTitle,work} = userActivity3;
+console.log(userActivity3.name,jobTitle,work);
